2305a5fb8d402ce6cb60f92345918f5e98a391b5
june <junetaylr@gmail.com>
2025-07-16 19:19:23 -0400

    Initial Commit

---
+++ globalleague/globalleagueApp.swift
@@ -0,0 +1,17 @@
+//
+//  globalleagueApp.swift
+//  globalleague
+//
+//  Created by june taylr on 7/16/25.
+//
+
+import SwiftUI
+
+@main
+struct globalleagueApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ globalleagueTests/globalleagueTests.swift
@@ -0,0 +1,17 @@
+//
+//  globalleagueTests.swift
+//  globalleagueTests
+//
+//  Created by june taylr on 7/16/25.
+//
+
+import Testing
+@testable import globalleague
+
+struct globalleagueTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
